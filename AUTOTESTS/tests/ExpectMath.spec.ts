bd02a946978c8eb61f364db6c31db5ed37557484
Maxim <relax8.99@mail.ru>
2025-04-17 14:21:56 +0300

    UnitTests

--- AUTOTESTS/tests/ExpectMath.spec.ts
+++ AUTOTESTS/tests/ExpectMath.spec.ts
@@ -1,21 +1,68 @@
 import { test, expect } from '@playwright/test'
 import { summury } from '../../lessons1/lessons1'
 import { compareNumbers } from '../../lessons1/lessons1'
+import { chechSumSign } from '../../lessons1/lessons1'
+import { printColor } from '../../lessons1/lessons1'
+import { isPositive } from '../../lessons1/lessons1'
+import { isNegative } from '../../lessons1/lessons1'
+import { visokosGod } from '../../lessons1/lessons1'
+import { massive } from '../../lessons1/lessons1'
+import { cyrcleMassive } from '../../lessons1/lessons1'
+import { doubleMassive } from '../../lessons1/lessons1'
 import { Student } from "../../lessons4/students"
 
 test('Expect Math Formuls', () => {
-expect(summury(10, 11)).toBeFalsy()
-
+    expect(summury(10, 11)).toBeFalsy()
+    expect(summury(9, 8)).toBeTruthy()
 })
 
 test('Expect Math compareNumbers', () => {
-expect(compareNumbers()).toBeFalsy()
+    expect(compareNumbers()).toBeFalsy()
+})
 
+test('chechSumSign', () => {
+    expect(chechSumSign()).toBeTruthy()
+})
+
+test('printColor' , () => {
+    expect(printColor(150)).toBe('green')
+    expect(printColor(50)).toEqual('yellow')
+    expect(printColor(150)).toEqual('green')
+})
+
+test('isPositive', () => {
+    expect.soft(isPositive(-1)).toBe('negative')
+    expect.soft(isPositive(0)).toBe('positive')
+    expect.soft(isPositive(1)).toBe('positive')
+})
+
+test('isNegative' , () => {
+    expect.soft(isNegative(5)).toBeTruthy()
+    expect.soft(isNegative(-5)).toBeFalsy()
+    expect.soft(isNegative(0)).toBeTruthy()
+})
+
+test('visokosGod', () => {
+    expect.soft(visokosGod(100)).toBe('nevisokos')
+    expect.soft(visokosGod(400)).toBe('visokos')
+    expect.soft(visokosGod(501)).toBe('nevisokos')
+})
+
+test('massive', () => {
+    expect.soft(massive()).toEqual([1,0,0,1,0,1,1])
+})
+
+test('cyrcleMassive', () => {
+    expect.soft(cyrcleMassive()).toEqual([2,10,6,4,11,8,10,4,8,8,9,2])
+})
+
+test('doubleMassive', () => { 
+    expect.soft(doubleMassive()).toEqual([[1,0,0,0,1],[0,1,0,1,0],[0,0,1,0,0],[0,1,0,1,0],[1,0,0,0,1]])
 })
 
 test('SetName', () => {
-let student1: Student = new Student('Alesha', '555', 5, 1)
-expect(student1.name).toEqual('Alesha')
-student1.setName('ALex')
-expect(student1.name).toEqual('ALex')
+    let student1: Student = new Student('Alesha', '555', 5, 1)
+    expect(student1.name).toEqual('Alesha')
+    student1.setName('ALex')
+    expect(student1.name).toEqual('ALex')
 })
--- lessons1/lessons1.ts
+++ lessons1/lessons1.ts
@@ -5,91 +5,77 @@ export function words() {
    const c: string = 'Apple';
    console.log(`${a}\n${b}\n${c}`)
 }
-// words()
 
 export function chechSumSign() {
     let a: number = 5
     let b: number = -10
     if(a+b>0){
-        console.log(a+b)
+        return a+b
     } else {
-        console.log('Я долбаеб')
+        return Error
     }  
 }
-// chechSumSign()
 
-export function printColor(){
-    let a: number = -50
+export function printColor(a: number){
     if(a<0){
-        console.log('red')
+        return 'red'
     } else if(0<=a && a<100){
-        console.log('yellow')
+        return 'yellow'
     } else {
-        console.log('green')
+        return 'green'
     }
 }
-// printColor()
 
 export function compareNumbers(){
     const a: number = 5
     const b: number = 6
     if(a>b){
-        // console.log('error')
         return true
     } else {
-        // console.log('success')
         return false
     }
 }
-// compareNumbers()
 
 export function summury(a:number, b: number){
     let sum: number = a+b
     if(10<=sum && sum<=20){
-        // console.log('true')
         return true
     } else {
-        // console.log('false')
         return false
     }
 }
-summury(10, 11)
 
 export function isPositive(a: number) {
     if(a>=0){
-        console.log('positive')
+        return 'positive'
     } else {
-        console.log('negative')
+        return 'negative'
     }
   }
-//   isPositive(0)
 
 export function isNegative(a: number) {
     if(a>=0){
-        console.log(a>0)
+        return true
     } else {
-        console.log(a<0)
+        return false
     }
   }
-// isNegative(0)
 
 export function several(a: number, b: string){
     for(let i=1; i<=a; i++){
         console.log(b)
     }
 }
-// several(5, 'AsyA')
 
 export function visokosGod(god: number){
     if(god % 4 == 0 && god % 400 == 0){
-        console.log("god - visokos")
+        return 'visokos'
     } else if(god % 100 == 0){
-        console.log('god - nevisokos')
+        return 'nevisokos'
     } else {
-        console.log('god - nevisokos')
+        return 'nevisokos'
     }
 }
-// visokosGod(500)
 
 export function massive(){
     const a: Array<number> = [0,1,1,0,1,0,0]
@@ -100,9 +86,8 @@ export function massive(){
             a[i] = 0
         }
     }
-    console.log(a)
+    return a
 }
-// massive()
 
 export function emptyMassive(){
     const a: Array<number> = [100]
@@ -111,7 +96,6 @@ export function emptyMassive(){
     }
     console.log(a)
 }
-// emptyMassive()
 
 export function cyrcleMassive(){
     const a: Array<number> = [1,5,3,2,11,4,5,2,4,8,9,1]
@@ -119,9 +103,8 @@ export function cyrcleMassive(){
         if(a[i]<6)
             a[i] = a[i]*2
     }
-    console.log(a)
+    return a
 }
-// cyrcleMassive()
 
 export function doubleMassive(){
     const a = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
@@ -132,6 +115,5 @@ export function doubleMassive(){
               } 
               a[i][a.length-1-i]=1;
     }
-    console.log(a)
+    return a
 }
-// doubleMassive()
