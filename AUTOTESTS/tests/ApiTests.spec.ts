132aa31e4b8a4fd163ebab34a4dd33ed99095c28
Maxim <relax8.99@mail.ru>
2025-04-26 15:18:24 +0300

    addApi

---
+++ AUTOTESTS/tests/ApiTests.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test'
+
+test.only('Check user not found error', async ({ request }) => {
+  const newIssue = await request.post('https://demoqa.com/Account/v1/Authorized', {
+    data: {
+      userName: 'maksimenko',
+      password: 'eifrjdf77'
+    }
+  })
+  expect(newIssue.ok()).toBeFalsy()
+  expect(newIssue.statusText()).toBe('Not Found')
+  expect(newIssue.status()).toBe(404)
+})
+
+test.only('Check amount of books in v1/Books', async ({ request }) => {
+  const newIssue = await request.get('https://demoqa.com/BookStore/v1/Books')
+  expect(newIssue.body()).toBeDefined()
+  const books = JSON.parse((await newIssue.body()).toString())
+  expect(books.books.length).toEqual(8)
+})
